53acfea80091b579aae88e76366f05d1bfd899b8
sonia <s.rolland@codeur.online>
2021-05-05 11:45:07 +0200

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voyages</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="header">
+  <a href="DreamTours" class="title">DreamTours</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+    <li><a href="#Locations">Locations</a></li>
+    <li><a href="#Montagnes">Montagne</a></li>
+    <li><a href="#Campagnes">Campagne</a></li>
+    <li><a href="#Mer">Mer</a></li>
+    <li><a href="#Contact">Contact</a></li>
+  </ul>
+</header>
+</body>
+</html>
